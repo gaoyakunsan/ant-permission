6dffdebc5e76f836527fe14d1409cf9eb4bcc380
gaoyakun <zzuygk@163.com>
2020-09-20 20:41:03 +0800

    集成sharding-jdbc实现读写分离

---
+++ channel-core/src/main/java/com/gao/channelcore/business/masterSlave/ShardingDataSourceConfig.java
@@ -0,0 +1,60 @@
+package com.gao.channelcore.business.masterSlave;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.google.common.collect.Maps;
+import io.shardingsphere.core.api.MasterSlaveDataSourceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+@EnableConfigurationProperties(ShardingMasterSlaveConfig.class)
+@ConditionalOnProperty({"sharding.jdbc.data-sources.ds_master.url", "sharding.jdbc.master-slave-rule.master-data-source-name"})
+public class ShardingDataSourceConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(ShardingDataSourceConfig.class);
+
+    @Autowired(required = false)
+    private ShardingMasterSlaveConfig shardingMasterSlaveConfig;
+
+    @Bean("dataSource")
+    public DataSource masterSlaveDataSource() throws SQLException {
+        shardingMasterSlaveConfig.getDataSources().forEach((k, v) -> configDataSource(v));
+        Map<String, DataSource> dataSourceMap = Maps.newHashMap();
+        dataSourceMap.putAll(shardingMasterSlaveConfig.getDataSources());
+        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource(dataSourceMap, shardingMasterSlaveConfig.getMasterSlaveRule(), new HashMap<>(), new Properties());
+        logger.info("masterSlaveDataSource config complete");
+        return dataSource;
+    }
+
+    private void configDataSource(DruidDataSource druidDataSource) {
+        druidDataSource.setMaxActive(20);
+        druidDataSource.setInitialSize(1);
+        druidDataSource.setMaxWait(60000);
+        druidDataSource.setMinIdle(1);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+        druidDataSource.setMinEvictableIdleTimeMillis(300000);
+        druidDataSource.setValidationQuery("select 1 from dual");
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setPoolPreparedStatements(true);
+        druidDataSource.setMaxOpenPreparedStatements(20);
+        druidDataSource.setUseGlobalDataSourceStat(true);
+        try {
+            druidDataSource.setFilters("stat,wall,slf4j");
+        } catch (SQLException e) {
+            logger.error("druid configuration initialization filter", e);
+        }
+    }
+}
